60421aa5c14d6842aa4147c18bc179bb774b0003
王岩 <315259756@qq.com>
2018-12-05 09:32:10 +0800

    新建项目

---
+++ SocialSecurity/SocialSecurity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialSecurity
+//
+//  Created by 王岩 on 2018/12/5.
+//  Copyright © 2018年 王岩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SocialSecurity/SocialSecurityTests/SocialSecurityTests.swift
@@ -0,0 +1,34 @@
+//
+//  SocialSecurityTests.swift
+//  SocialSecurityTests
+//
+//  Created by 王岩 on 2018/12/5.
+//  Copyright © 2018年 王岩. All rights reserved.
+//
+
+import XCTest
+@testable import SocialSecurity
+
+class SocialSecurityTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
